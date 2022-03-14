eba65016d48b8c194e15179b937bef661cef537b
Anders Hesselbom <anders@winsoft.se>
2022-03-14 18:51:27 +0100

    Added staff.

--- CreateTestData.sql
+++ CreateTestData.sql
@@ -25,3 +25,6 @@ EXEC dbo.RegisterCassette @ID, 511734916656
 
 EXEC dbo.RegisterCustomer 'Sven Persson', '112233-4455', '', '', '', '', '', ''
 EXEC dbo.RegisterCustomer 'Conny Karlsson', '223344-5566', '', '', '', '', '', ''
+
+EXEC dbo.RegisterStaff 'Andy McAndy', '334455-6677'
+EXEC dbo.RegisterStaff 'Pandy McPandy', '445566-7788'
