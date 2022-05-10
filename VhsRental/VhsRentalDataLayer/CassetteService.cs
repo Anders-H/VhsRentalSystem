b4e847ca2784f65624d224ff913202cabbc5ea5d
Anders-H <anders@winsoft.se>
2022-05-10 19:44:33 +0200

    More work on the rental process.

--- VhsRental/VhsRentalDataLayer/CassetteService.cs
+++ VhsRental/VhsRentalDataLayer/CassetteService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
 using VhsRentalDataLayer.Entities;
 
 namespace VhsRentalDataLayer;
@@ -15,17 +16,25 @@ public class CassetteService : IDisposable
 
     public bool CassetteIsOut(int cassetteId)
     {
-        // dbo.IsCassetteOut
+        using var cmd = new SqlCommand("dbo.IsCassetteOut", _connection);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@ID", cassetteId);
+        return (int)cmd.ExecuteScalar() > 0;
     }
 
     public void ReturnCassette(int cassetteId, int staffId, string description)
     {
-
+        using var cmd = new SqlCommand("dbo.ReturnCassette", _connection);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@ID", cassetteId);
+        cmd.Parameters.AddWithValue("@StaffID", staffId);
+        cmd.Parameters.AddWithValue("@ID", cassetteId);
+        cmd.ExecuteNonQuery();
     }
 
     public RentalCassette? GetCassetteForRental(int cassetteId)
     {
-
+        //dbo.SuggestPrice
     }
 
     public void Dispose()
